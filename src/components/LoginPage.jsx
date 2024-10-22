f916a86db57bb5ed7ba353d5cffed103027de353
deepakkumar1211 <sahudeepak1211.com>
2024-10-22 14:31:24 +0530

    previous login made by raj

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,95 +1,3 @@
-// // src/components/LoginPage.jsx
-// import React, { useState } from "react";
-// import { useDispatch } from 'react-redux';
-// import { useNavigate } from "react-router-dom";
-// import { login } from '../redux/authSlice';
-// import "./LoginPage.css";
-// import { NavLink } from "react-router-dom";
-
-// const LoginPage = () => {
-//   const navigate = useNavigate();
-//   const dispatch = useDispatch();
-//   const [formData, setFormData] = useState({ email: '', password: '' });
-//   const [errMsg, setErrMsg] = useState('');
-
-//   const handleChange = (e) => {
-//     const { id, value } = e.target;
-//     setFormData((prevData) => ({ ...prevData, [id]: value }));
-//   };
-
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     const requestBody = {
-//       email: formData.email,
-//       password: formData.password
-//     };
-
-//     try {
-//       const response = await fetch('https://vivacious-stillness-production.up.railway.app/v1/users/login', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify(requestBody),
-//       });
-
-//       const data = await response.json();
-
-//       if (data.user && data.token) {
-//         dispatch(login({ user: data.user, token: data.token }));
-//         navigate('/home');
-//       } else {
-//         setErrMsg('Invalid Username or Password');
-//       }
-//     } catch (error) {
-//       console.error('Error:', error);
-//       setErrMsg('An error occurred. Please try again.');
-//     }
-//   };
-
-//   return (
-//     <main className="login-header w-full">
-//       <nav className="p-6 font-bold text-4xl text-start text-gray-200">
-//         <h1>Connectify</h1>
-//       </nav>
-//       <div className="login-container">
-//         <nav className="mx-auto w-40">
-//           <img src="https://connectify.me/wp-content/uploads/HOTSPOT-2021-01.png" alt="logo" className="logo" />
-//         </nav>
-//         <h2 className="my-6 text-3xl font-semibold text-gray-300 sm:text-black">Sign In</h2>
-//         {errMsg && <div className="text-red-600">{errMsg}</div>}
-//         <form onSubmit={handleSubmit}>
-//           <div className="input-field mt-4 w-full h-14 bg-white rounded-sm relative overflow-hidden">
-//             <input type="email" id="email" value={formData.email} onChange={handleChange} required />
-//             <label htmlFor="email">Email </label>
-//           </div>
-//           <div className="input-field mt-4 w-full h-14 bg-white rounded-sm relative">
-//             <input type="password" id="password" value={formData.password} onChange={handleChange} required />
-//             <label htmlFor="password">Password</label>
-//           </div>
-//           <input type="submit" value="Sign In" className="hover:cursor-pointer" />
-//           <div className="remember">
-//             <div className="checkbox">
-//               <input type="checkbox" defaultChecked className="cursor-pointer" />
-//               <label htmlFor="checkbox"> Remember me</label>
-//             </div>
-//             <span>Need help?</span>
-//           </div>
-//         </form>
-//         <p>
-//           New to Connectify? <NavLink to="/signup">Sign up now.</NavLink>
-//         </p>
-        
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default LoginPage;
-
-
-
-
 // src/components/LoginPage.jsx
 import React, { useState } from "react";
 import { useDispatch } from 'react-redux';
@@ -109,6 +17,7 @@ const LoginPage = () => {
     setFormData((prevData) => ({ ...prevData, [id]: value }));
   };
 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -118,27 +27,17 @@ const LoginPage = () => {
     };
 
     try {
-      const response = await fetch('https://connectify-backend-2uq0.onrender.com/api/v1/users/login', {
+      const response = await fetch('https://vivacious-stillness-production.up.railway.app/v1/users/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(requestBody),
       });
 
       const data = await response.json();
-<<<<<<< HEAD
-      console.log("response:", data);
-      
-
-      if (data) {
-        // dispatch(login({ user: data.user, token: data.token }));
-        navigate('/home');
-=======
-      console.log(data);
 
       if (data.user && data.token) {
         dispatch(login({ user: data.user, token: data.token }));
-        navigate('/home'); // Redirect to home page after successful login
->>>>>>> 1bc91a2a9d77a25d6201b42148a021332b3123ad
+        navigate('/home');
       } else {
         setErrMsg('Invalid Username or Password');
       }
@@ -180,6 +79,7 @@ const LoginPage = () => {
         <p>
           New to Connectify? <NavLink to="/signup">Sign up now.</NavLink>
         </p>
+        
       </div>
     </main>
   );
