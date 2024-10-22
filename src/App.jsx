bed47124ea90c516ab014c16845e64d073404149
deepakkumar1211 <sahudeepak1211.com>
2024-10-22 14:54:30 +0530

    "error fixes of app page"

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,16 @@ const App = () => {
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
-<<<<<<< HEAD
+
       <Route path="/home" element={ <Home /> }/>
       <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
       <Route path="account-info" element={isAuthenticated ? <AccountInformation /> : <Navigate to="/login" />} />
       <Route path="default-settings" element={isAuthenticated ? <DefaultSettings /> : <Navigate to="/login" />} />
-=======
+
       <Route path="/home" element={isAuthenticated ? <Home /> : <LoginPage />} />
       <Route path="account-info" element={<AccountInformation />} />
       <Route path="settings" element={isAuthenticated ? <DefaultSettings /> : <LoginPage />} />
       {/* <Route path="account-info" element={isAuthenticated ? <AccountInformation /> : <Navigate to="/login" />} /> */}
->>>>>>> 1bc91a2a9d77a25d6201b42148a021332b3123ad
       <Route path="/" element={<Navigate to={isAuthenticated ? "/home" : "/login"} />} />
     </Routes>
   );
