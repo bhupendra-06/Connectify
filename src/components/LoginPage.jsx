4459877cea63cd940305a5a7d59e2e8d78c261ac
Bhupendra <bhupendra.scripts@gmail.com>
2025-01-16 15:38:04 +0530

    implemented Sign in loader

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -4,54 +4,62 @@ import { useNavigate } from "react-router-dom";
 import "./LoginPage.css";
 import { NavLink } from "react-router-dom";
 import Cookies from "js-cookie";
-
+import { ClipLoader } from "react-spinners";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-  const [formData, setFormData] = useState({ email: '', password: '' });
-  const [errMsg, setErrMsg] = useState('');
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [errMsg, setErrMsg] = useState("");
 
   const handleChange = (e) => {
     const { id, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [id]: value }));
   };
 
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     const requestBody = {
       email: formData.email,
       password: formData.password,
     };
-  
+
     try {
-      const response = await fetch("https://connectify-backend-2uq0.onrender.com/api/v1/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(requestBody),
-      });
-  
+      setLoading(true);
+      const response = await fetch(
+        "https://connectify-backend-2uq0.onrender.com/api/v1/users/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(requestBody),
+        }
+      );
+
       const data = await response.json();
-      console.log("data",data);
+      console.log("data", data);
       // console.log("MyUserID :",data.data.user._id);
-  
-      if (data.statusCode === 200 && data.data.accessToken && data.data.refreshToken) {
-        
+
+      if (
+        data.statusCode === 200 &&
+        data.data.accessToken &&
+        data.data.refreshToken
+      ) {
         Cookies.set("accessToken", data.data.accessToken, { expires: 1 }); // Expires in 1 day
         Cookies.set("refreshToken", data.data.refreshToken, { expires: 7 }); // Expires in 7 days
         Cookies.set("MyOwnerId", data.data.user._id, { expires: 7 }); // Owner Id for use
         Cookies.set("avatar", data.data.user.avatar, { expires: 7 });
-  
+
         navigate("/home");
-        
       } else {
         setErrMsg("Invalid Username or Password");
       }
     } catch (error) {
       console.error("Error:", error);
       setErrMsg("Invalid Username or Password. Please try again.");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -62,32 +70,57 @@ const LoginPage = () => {
       </nav>
       <div className="login-container">
         <nav className="mx-auto w-40">
-          <img src="https://connectify.me/wp-content/uploads/HOTSPOT-2021-01.png" alt="logo" className="logo" />
+          <img
+            src="https://connectify.me/wp-content/uploads/HOTSPOT-2021-01.png"
+            alt="logo"
+            className="logo"
+          />
         </nav>
-        <h2 className="my-6 text-3xl font-semibold text-gray-300 sm:text-black">Sign In</h2>
+        <h2 className="my-6 text-3xl font-semibold text-gray-300 sm:text-black">
+          Sign In
+        </h2>
         {errMsg && <div className="text-red-600">{errMsg}</div>}
         <form onSubmit={handleSubmit}>
           <div className="input-field mt-4 w-full h-14 bg-white rounded-sm relative overflow-hidden">
-            <input type="email" id="email" value={formData.email} onChange={handleChange} required />
+            <input
+              type="email"
+              id="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
             <label htmlFor="email">Email </label>
           </div>
           <div className="input-field mt-4 w-full h-14 bg-white rounded-sm relative">
-            <input type="password" id="password" value={formData.password} onChange={handleChange} required />
+            <input
+              type="password"
+              id="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
             <label htmlFor="password">Password</label>
           </div>
-          <input type="submit" value="Sign In" className="hover:cursor-pointer" />
+          <div className="mt-10 px-5 py-3 w-full text-center font-bold text-white bg-blue-600 rounded-2xl">
+            {loading ? (
+              <ClipLoader size={16} color="white" className="mx-2" />
+            ) : (
+              <button type="submit" className="hover:cursor-pointer">
+                Sign In
+              </button>
+            )}
+          </div>
           <div className="remember">
-            <div className="checkbox">
-              <input type="checkbox" defaultChecked className="cursor-pointer" />
+            <div className="checkbox flex gap-1 items-center">
+              <input type="checkbox" className="cursor-pointer" />
               <label htmlFor="checkbox"> Remember me</label>
             </div>
-            <span>Need help?</span>
+            <span className="hover:text-blue-600">Need help?</span>
           </div>
         </form>
         <p>
           New to Connectify? <NavLink to="/signup">Sign up now.</NavLink>
         </p>
-        
       </div>
     </main>
   );
--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -3,23 +3,23 @@ import "./LoginPage.css";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const SignUpPage = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
-    email: '',
-    fullName: '',
-    username: '',
-    password: ''
+    email: "",
+    fullName: "",
+    username: "",
+    password: "",
   });
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
+  const [msgColor, setMsgColor] = useState("");
 
   const handleChange = (e) => {
     const { id, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [id]: value
+      [id]: value,
     }));
   };
 
@@ -37,39 +37,46 @@ const SignUpPage = () => {
       fullName: formData.fullName,
       email: formData.email,
       username: formData.username,
-      password: formData.password
+      password: formData.password,
     };
-    console.log("requestBody",requestBody);
+    console.log("requestBody", requestBody);
 
     // console.log(requestBody);
 
-    fetch('https://connectify-backend-2uq0.onrender.com/api/v1/users/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(requestBody)
-    })
-      .then(response => {
+    fetch(
+      "https://connectify-backend-2uq0.onrender.com/api/v1/users/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      }
+    )
+      .then((response) => {
         if (!response) {
-          throw new Error('Network response was not ok ' + response.statusText);
+          throw new Error("Network response was not ok " + response.statusText);
         }
         return response.json();
       })
-      .then(data => {
-        console.log('Response data:', data);
+      .then((data) => {
+        console.log("Response data:", data);
         if (data) {
-          setMessage('Registration successfully Done!');
+          setMessage("Registration successfully Done!");
+          setMsgColor("text-green-600");
+          alert("Registration successfully Done!");
+          navigate('/login');
         } else {
-          setMessage('Registration failed: Unexpected response format');
+          setMessage("Registration failed: Unexpected response format");
+          setMsgColor("text-red-600");
         }
       })
-      .catch(error => {
-        console.error('Error:', error);
-        setMessage('Registration failed: ' + error.message);
+      .catch((error) => {
+        console.error("Error:", error);
+        setMessage("Registration failed: " + error.message);
+        setMsgColor("text-red-600");
       });
 
-      navigate('/login');
   };
 
   return (
@@ -79,8 +86,13 @@ const SignUpPage = () => {
           <h1>Connectify</h1>
         </nav>
         <div className="login-container">
-          <h2 className="my-6 text-3xl font-semibold text-gray-300 sm:text-black">Sign Up</h2>
-          {message && <div className="text-green-600 text-xl font-bold">{message}</div>} {/* Display message */}
+          <h2 className="my-6 text-3xl font-semibold text-gray-300 sm:text-black">
+            Sign Up
+          </h2>
+          {message && (
+            <div className={`${msgColor} text-xl font-bold`}>{message}</div>
+          )}{" "}
+          {/* Display message */}
           <form onSubmit={handleSubmit}>
             <div className="input-field mt-4 w-full h-14 bg-white rounded-sm relative overflow-hidden">
               <input
@@ -120,19 +132,25 @@ const SignUpPage = () => {
                 onChange={handleChange}
                 required
               />
-              <label htmlFor="password">Password</label>
+              <label htmlFor="password">Create Password</label>
             </div>
-            <input type="submit" value="Sign Up" className="hover:cursor-pointer" />
+            <div className="mt-10 px-5 py-3 w-full text-center font-bold text-white bg-blue-600 rounded-2xl hover:cursor-pointer">
+              <button type="submit">Sign Up</button>
+            </div>
             <div className="remember">
               <div className="checkbox">
-                <input type="checkbox" defaultChecked className="cursor-pointer" />
-                <label htmlFor="checkbox"> Remember me</label>
+                <input
+                  type="checkbox"
+                  defaultChecked
+                  className="cursor-pointer"
+                />
+                <label htmlFor="checkbox">Remember me</label>
               </div>
               <span>Need help?</span>
             </div>
           </form>
           <p>
-            Already have an account? <NavLink to="/login" >Sign In.</NavLink>
+            Already have an account? <NavLink to="/login">Sign In.</NavLink>
           </p>
         </div>
       </main>
